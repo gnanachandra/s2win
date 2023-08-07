5c552b2fc7682285f8f72268aed8f74e1e3202c0
gnanachandra <vvit.20bq1a05p2@gmail.com>
2023-08-07 08:24:01 +0530

    updated axios.js

--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios"
+import axios from "../api/axios"
 import { toast } from "react-hot-toast";
 
 //getclients
